3ca0c0046335b36d759ba98cf4a01c4b57b6e976
Victor Costan <costan@gmail.com>
2014-02-03 05:34:12 -0500

    Replace gems that don't build in MRI 2.0.

--- lib/rails_pwnerer/scaffolds/gems.rb
+++ lib/rails_pwnerer/scaffolds/gems.rb
@@ -2,7 +2,7 @@
 
 class RailsPwnerer::Scaffolds::Gems
   include RailsPwnerer::Base
-  
+
   def install_self
     # The gem repository gets wiped on new Debian and Ubuntu installs, so we
     # need to reinstall ourselves.
@@ -11,31 +11,31 @@ class RailsPwnerer::Scaffolds::Gems
     # Process management.
     install_gems %w(zerg_support)
   end
-  
+
   def install_databases
     install_gems %w(mysql mysql2 pg sqlite3)
     install_gems %w(memcache-client)
   end
-  
+
   def install_packagers
     install_gems %w(rake thor bundler)
   end
-  
+
   def install_servers
     install_gems %w(thin unicorn)
   end
-  
-  def install_tools    
+
+  def install_tools
     # Get passwords from admins.
-    install_gems %w(highline termios)
+    install_gems %w(highline ruby-termios)
 
     # Debug gems on production machines.
-    install_gems %w(echoe jeweler ruby-debug ruby-debug19 ruby-debug-ide)
-    
+    install_gems %w(echoe jeweler debugger)
+
     # Determine number of CPUs and cores.
     install_gems %w(sys-cpu)
   end
-  
+
   # runner
   def run
     update_gems
@@ -46,7 +46,7 @@ class RailsPwnerer::Scaffolds::Gems
     install_tools
     install_gems %w(rails)
   end
-  
+
   # standalone runner
   def self.go
     self.new.run
