2dee860b80a9a23274f0646ae2722e039fe30ec3
Victor Costan <costan@gmail.com>
2014-02-03 14:17:05 -0500

    Skip development / test gems in bundle install.

--- lib/rails_pwnerer/app/gems.rb
+++ lib/rails_pwnerer/app/gems.rb
@@ -2,10 +2,10 @@
 
 class RailsPwnerer::App::Gems
   include RailsPwnerer::Base
-  
+
   def update(app_name, instance_name)
     app_config = RailsPwnerer::Config[app_name, instance_name]
-    
+
     Dir.chdir app_config[:app_path] do
       # Phase 1: app-directed install
       if !File.exist?('Gemfile') && app_config[:gems]
@@ -22,18 +22,18 @@ class RailsPwnerer::App::Gems
           end
         end
       end
-    
+
       # Phase 2: bundler / rails install
       # Install the gems needed by the app.
       if File.exist? 'Gemfile'
         unless /^\s+gem\s+['"]thin['"]/ =~ File.read('Gemfile')
           File.open('Gemfile', 'a') { |f| f.write "\ngem 'thin'\n"}
         end
-        Kernel.system "bundle install"
+        Kernel.system "bundle install --without development test"
       else
         Kernel.system "rake gems:install RAILS_ENV=production"
       end
-    end    
+    end
   end
 
   def manage(app_name, instance_name, action)
